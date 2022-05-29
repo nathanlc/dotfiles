1afc5bdf555d8ee9e187f85c71b0a1ebf01c1060
Nathan Lecourt <nathan.lecourt@gmail.com>
2022-05-29 22:00:13 +0200

    Add app manipulations in hammerspoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,7 +1,26 @@
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "w", function()
-  -- hs.alert.show("Hello World!")
-    hs.notify.new({title="Hammerspoon", informativeText="Hello World"}):send()
+-- Application manipulations
+-- Ctrl + 1 is not working for some reason. It worked in Quicksilver
+-- hs.hotkey.bind({"ctrl"}, "1", function()
+    -- hs.application.launchOrFocus("iTerm")
+-- end)
+hs.hotkey.bind({"ctrl"}, "²", function()
+    hs.application.launchOrFocus("iTerm")
 end)
+hs.hotkey.bind({"ctrl"}, "2", function()
+    hs.application.launchOrFocus("Vivaldi Snapshot")
+end)
+
+hs.hotkey.bind({"ctrl"}, "3", function()
+    hs.application.launchOrFocus("Slack")
+end)
+
+hs.hotkey.bind({"ctrl"}, "4", function()
+    hs.application.launchOrFocus("zoom.us")
+end)
+
+
+-- Window manipulations
+hs.window.animationDuration = 0.1
 
 hs.hotkey.bind({"cmd", "alt"}, "H", function()
     local window = hs.window.focusedWindow()
@@ -30,12 +49,6 @@ end)
 
 hs.hotkey.bind({"cmd", "alt"}, "M", function()
     local window = hs.window.focusedWindow()
-    local frame = window:frame()
-    local screenFrame = window:screen():frame()
 
-    frame.x = screenFrame.x
-    frame.y = screenFrame.y
-    frame.w = screenFrame.w
-    frame.h = screenFrame.h
-    window:setFrame(frame)
+    window:maximize()
 end)
