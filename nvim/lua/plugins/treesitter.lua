7ab4cafcb5d9d4a41ada32709366d1653a1261dc
Nathan Lecourt <nathan.lecourt@gmail.com>
2022-06-07 09:27:17 +0200

    Update treesitter config for nvim

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 local treesitter = require('nvim-treesitter')
 
 require('nvim-treesitter.configs').setup({
-    ensure_installed = 'maintained',
+    ensure_installed = 'all',
     highlight = {
       enable = true,
     },
