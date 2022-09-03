bf579bd21cd11489ad214b988ba6eba414dceb5f
Nathan Lecourt <nathan.lecourt@gmail.com>
2022-09-03 14:30:55 +0200

    [nvim] Change package manager

--- nvim/lua/package-manager.lua
+++ nvim/lua/package-manager.lua
@@ -1,56 +1,67 @@
-require('paq') {
-    'savq/paq-nvim';
-    'nvim-treesitter/nvim-treesitter';
-    'nvim-treesitter/nvim-treesitter-textobjects';
-    'nvim-treesitter/nvim-treesitter-context';
-    'RRethy/nvim-treesitter-endwise';
-    'kyazdani42/nvim-web-devicons';
-    'nvim-lua/plenary.nvim'; -- Mostly lua helper functions
-    'neovim/nvim-lspconfig';
-    'mfussenegger/nvim-jdtls'; -- Lsp for Java. Download java language server jdtls from https://download.eclipse.org/jdtls/snapshots/?d and extract in chosen path
-    -- nvim-cmp start Auto completion compatible with LSP
-    'hrsh7th/cmp-nvim-lsp';
-    'hrsh7th/cmp-buffer';
-    'hrsh7th/cmp-path';
-    'hrsh7th/cmp-cmdline';
-    'hrsh7th/nvim-cmp';
-    -- nvim-cmp end
-    -- Telescope start
-    'nvim-lua/popup.nvim';
-    'nvim-lua/plenary.nvim';
-    'ggandor/leap.nvim';
-    'jghauser/mkdir.nvim';
-    'tpope/vim-sleuth';
-    'tpope/vim-vinegar';
-    'andymass/vim-matchup';
-    'nvim-telescope/telescope.nvim';
-    'nvim-telescope/telescope-fzy-native.nvim';
-    'BurntSushi/ripgrep';
-    -- Telescope end
-    'christoomey/vim-tmux-navigator';
-    'tpope/vim-rsi';
-    'tpope/vim-dispatch';
-    -- Themes start
-    -- 'shaunsingh/nord.nvim';
-    'navarasu/onedark.nvim';
-    -- Themes end
-    'nanotee/sqls.nvim';
-    'tpope/vim-dadbod';
-    'tpope/vim-rails';
-    'jiangmiao/auto-pairs';
-    'preservim/nerdcommenter';
-    'tpope/vim-projectionist';
-    'tpope/vim-surround';
-    'tpope/vim-abolish'; -- Smartcase string substitutions
-    -- 'tpope/vim-fugitive';
-    -- Neogit start
-    'TimUntersberger/neogit';
-    'sindrets/diffview.nvim';
-    -- Neogit end
-    'tpope/vim-fugitive';
-    'nvim-orgmode/orgmode';
-    'gcmt/taboo.vim';
-    'L3MON4D3/LuaSnip';
-    'saadparwaiz1/cmp_luasnip';
-    'rafamadriz/friendly-snippets';
-}
+local packer = require('packer')
+
+return packer.startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'nvim-lua/plenary.nvim' -- Mostly lua helper functions
+  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
+  use { 'nvim-treesitter/nvim-treesitter-textobjects', requires = 'nvim-treesitter/nvim-treesitter' }
+  use { 'nvim-treesitter/nvim-treesitter-context', requires = 'nvim-treesitter/nvim-treesitter' }
+  use { 'RRethy/nvim-treesitter-endwise', requires = 'nvim-treesitter/nvim-treesitter' }
+  use 'kyazdani42/nvim-web-devicons'
+  use 'neovim/nvim-lspconfig'
+  use 'mfussenegger/nvim-jdtls' -- Lsp for Java. Download java language server jdtls from https://download.eclipse.org/jdtls/snapshots/?d and extract in chosen path
+  -- nvim-cmp start Auto completion compatible with LSP
+  use 'hrsh7th/cmp-nvim-lsp'
+  use 'hrsh7th/cmp-buffer'
+  use 'hrsh7th/cmp-path'
+  use 'hrsh7th/cmp-cmdline'
+  use 'hrsh7th/cmp-nvim-lua'
+  use 'hrsh7th/nvim-cmp'
+  use 'saadparwaiz1/cmp_luasnip'
+  use 'onsails/lspkind.nvim'
+  -- nvim-cmp end
+  use {'L3MON4D3/LuaSnip', requires = 'saadparwaiz1/cmp_luasnip'}
+  -- Telescope start
+  use 'nvim-lua/popup.nvim'
+  use 'ggandor/leap.nvim'
+  use 'jghauser/mkdir.nvim'
+  use 'tpope/vim-sleuth'
+  use 'tpope/vim-vinegar'
+  use 'andymass/vim-matchup'
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-telescope/telescope-fzy-native.nvim'
+  use 'BurntSushi/ripgrep'
+  -- Telescope end
+  use 'christoomey/vim-tmux-navigator'
+  use 'tpope/vim-rsi'
+  use 'tpope/vim-dispatch'
+  -- Themes start
+  -- 'shaunsingh/nord.nvim'
+  use 'navarasu/onedark.nvim'
+  -- Themes end
+  use 'nanotee/sqls.nvim'
+  use 'tpope/vim-dadbod'
+  use 'tpope/vim-rails'
+  use 'jiangmiao/auto-pairs'
+  use 'preservim/nerdcommenter'
+  use 'tpope/vim-projectionist'
+  use 'tpope/vim-surround'
+  use 'tpope/vim-abolish' -- Smartcase string substitutions
+  -- 'tpope/vim-fugitive'
+  -- Neogit start
+  use 'TimUntersberger/neogit'
+  use 'sindrets/diffview.nvim'
+  -- Neogit end
+  use 'tpope/vim-fugitive'
+  use 'nvim-orgmode/orgmode'
+  use 'gcmt/taboo.vim'
+  use 'rafamadriz/friendly-snippets'
+  use 'mfussenegger/nvim-dap'
+  use '/Users/nathan/sandbox/mine/himalaya/vim'
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
+  packer.sync()
+end)
