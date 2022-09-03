723a5ccb70978e1b1f72c4c245d731623aa54841
Nathan Lecourt <nathan.lecourt@gmail.com>
2022-09-03 14:30:04 +0200

    [nvim] Missed to add file for project custom commands

---
+++ nvim/lua/utils/term.lua
@@ -0,0 +1,18 @@
+local Window = require('utils.window')
+
+local M = {}
+
+function M.open_small_term()
+  vim.api.nvim_command('split term://bash')
+  Window.make_small()
+  vim.opt_local.winfixheight = true
+end
+
+function M.run_in_term(command)
+  M.open_small_term()
+  local channel_id = vim.opt_local.channel:get()
+  vim.fn.chansend(channel_id, command .. '\n')
+  vim.api.nvim_win_set_cursor(0, {vim.api.nvim_buf_line_count(0), 0})
+end
+
+return M
